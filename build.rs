ba455a4abb459abc0849d9830582ee3f20cc2e44
Nazar Mishturak <nazarmx@gmail.com>
2020-09-16 01:30:17 +0300

    Add size_t_is_usize to build.rs

--- build.rs
+++ build.rs
@@ -17,6 +17,7 @@ fn run_bindgen(include_paths: &[PathBuf], defines: &[&str]) {
         .header("wrapper.h")
         .default_enum_style(bindgen::EnumVariation::NewType { is_bitfield: false })
         .whitelist_type("XML_.*")
+		.size_t_is_usize(true)
         .whitelist_var("XML_.*")
 		.whitelist_function("XML_.*")
         // Tell cargo to invalidate the built crate whenever any of the
